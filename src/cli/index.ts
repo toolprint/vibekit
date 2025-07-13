a8e7acbb0bde9093bb15206bb28434e76ade4457
Joe Danziger <joe@ticc.net>
2025-07-13 17:11:32 -0400

    use actual package version

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,13 +2,20 @@
 
 import { Command } from 'commander';
 import { initCommand } from './commands/init.js';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const packageJson = JSON.parse(readFileSync(join(__dirname, '../../package.json'), 'utf8'));
 
 const program = new Command();
 
 program
   .name('vibekit')
   .description('VibeKit development environment manager')
-  .version('0.1.0');
+  .version(packageJson.version);
 
 program
   .command('init')
